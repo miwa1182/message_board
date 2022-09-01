dd6dca925d5762ec4732423466a22c5ab7297721
miwako.kanda <miwa1182@outlook.com>
2022-09-01 23:04:21 +0900

    CreateServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -13,13 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import utils.DBUtil;
-
 /**
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,20 +28,19 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+            List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
 
-        em.close();
+            em.close();
 
-        request.setAttribute("messages", messages);
+            request.setAttribute("messages", messages);
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-        rd.forward(request, response);
-}
-
-}
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+            rd.forward(request, response);
+        }
+    }
